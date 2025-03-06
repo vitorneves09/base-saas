e9f6ad11fdf83c3163d503cb90b8670f7b454a42
Vitor Azevedo <vitor.azevedo@infracommerce.com.br>
2025-03-06 18:59:38 -0400

    init

---
+++ rector.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types = 1);
+
+return Rector\Config\RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/app',
+        __DIR__ . '/bootstrap/app.php',
+        __DIR__ . '/database',
+        __DIR__ . '/tests',
+        __DIR__ . '/routes',
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true,
+    )
+    ->withPhpSets();
