cf6828b8b1ef9ba36ea2e1ae4f0e9afce8c8712b
Vitor Azevedo <vitor.azevedo@infracommerce.com.br>
2025-03-06 19:34:37 -0400

    feat: add phpstan

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,16 +5,18 @@ declare(strict_types = 1);
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Override;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    #[\Override]
+    #[Override]
     public function register(): void
     {
-        //
+        //asas
+
     }
 
     /**
